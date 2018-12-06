2ff9dad00ac2ede007f7bb324731c1b4e33914c8
Haris Ghauri <hrghauri@gmail.com>
2018-12-06 02:25:50 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const api = {
+  baseUrl: "http://api.giphy.com/v1/gifs/trending",
+  api_key: "Your Api Key Here"
+};
+
+let limit = 10;
+let offset = 0;
+
+function addItems() {
+  let url =
+    api.baseUrl +
+    "?api_key=" +
+    api.api_key +
+    "&limit=" +
+    limit +
+    "&offset=" +
+    offset;
+
+  console.log(url);
+
+  fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      let newGifsData = data.data;
+      newGifsData.forEach(gifData => {
+        var div = document.createElement("div");
+        var image = document.createElement("IMG");
+        image.setAttribute("src", gifData.images.original.url);
+        image.setAttribute("height", 400);
+        image.setAttribute("width", 400);
+        div.appendChild(image);
+        var rootElement = document.getElementById("root");
+        rootElement.appendChild(div);
+      });
+    })
+    .catch(e => console.log(e.message));
+}
+
+function init() {
+  addItems();
+}
+
+window.onscroll = function(ev) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    offset = offset + limit;
+    console.log("scrolled at the bottom");
+    addItems();
+  }
+};
+
+init();
